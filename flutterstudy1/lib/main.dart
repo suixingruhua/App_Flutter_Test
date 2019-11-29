9e515830211e9fc821209166c1db08c199066ccb
gshs_wwx <1846531358@qq.com>
2019-11-29 17:00:47 +0800

    upv3

--- flutterstudy1/lib/main.dart
+++ flutterstudy1/lib/main.dart
@@ -13,23 +13,39 @@ class HomePage extends StatelessWidget {
         ),
         body: HomeContent(),
       ),
-      theme: ThemeData(
-        primarySwatch: Colors.yellow
-      ),
+      theme: ThemeData(primarySwatch: Colors.yellow),
     );
   }
 }
-class HomeContent extends StatelessWidget{
+
+class HomeContent extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Center(
+      child: Container(
         child: new Text(
-      "你好Flutter",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-        fontSize: 40.0,
-        color: Color.fromRGBO(244, 233, 121, 0.5)
+          "你好Flutter",
+          textAlign: TextAlign.left,
+          overflow: TextOverflow.ellipsis,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.lineThrough,
+              decorationColor: Colors.white,
+              decorationStyle: TextDecorationStyle.dashed
+          ),
+          maxLines: 1,
+          textScaleFactor: 2,
+        ),
+        width: 300.0,
+        height: 300.0,
+        decoration: BoxDecoration(
+            color: Colors.yellow,
+            border: Border.all(color: Colors.blue, width: 2.0)),
       ),
-    ));
+    );
   }
 }
