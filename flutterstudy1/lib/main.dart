e776a48a64157c2ee912dbca2baa99361f43a163
gshs_wwx <1846531358@qq.com>
2019-11-29 16:44:07 +0800

    upv2

--- flutterstudy1/lib/main.dart
+++ flutterstudy1/lib/main.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  return runApp(new Center(
-    child:new Text(
+void main() => runApp(new HomePage());
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: AppBar(
+          title: new Text("Flutter Dome"),
+        ),
+        body: HomeContent(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      ),
+    );
+  }
+}
+class HomeContent extends StatelessWidget{
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+        child: new Text(
       "你好Flutter",
       textDirection: TextDirection.ltr,
-    )
-  ));
-}
-class HomePage extends StatelessWidget{
-  
+      style: TextStyle(
+        fontSize: 40.0,
+        color: Color.fromRGBO(244, 233, 121, 0.5)
+      ),
+    ));
+  }
 }
