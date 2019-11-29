6f8e2253fd6fdb1b235fb31c6fff2aba4233192b
gshs_wwx <1846531358@qq.com>
2019-11-29 15:30:41 +0800

    upv1

---
+++ flutterstudy1/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  return runApp(new Center(
+    child:new Text(
+      "你好Flutter",
+      textDirection: TextDirection.ltr,
+    )
+  ));
+}
+class HomePage extends StatelessWidget{
+  
+}
